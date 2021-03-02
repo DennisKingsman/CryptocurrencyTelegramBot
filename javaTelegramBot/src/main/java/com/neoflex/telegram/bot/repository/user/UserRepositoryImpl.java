deae221edc3c7e7893260acbb0ddf45f1bfd1aa9
Dennis <den.kuznecov.1999@mail.ru>
2021-03-02 09:13:30 +0400

    dunno what happends

--- javaTelegramBot/src/main/java/com/neoflex/telegram/bot/repository/user/UserRepositoryImpl.java
+++ javaTelegramBot/src/main/java/com/neoflex/telegram/bot/repository/user/UserRepositoryImpl.java
@@ -5,10 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
