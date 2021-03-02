2c29075b4619bdd4f64534995cd80d2d09d6b399
GeorgeII <egorzorikov@gmail.com>
2021-03-02 11:41:22 +0400

    Multiple candles test added

--- service/candles/app/src/test/main/scala/rht/hack/dao/RedisCandleDaoTest.scala
+++ service/candles/app/src/test/main/scala/rht/hack/dao/RedisCandleDaoTest.scala
@@ -20,4 +20,28 @@ class RedisCandleDaoTest extends AnyFlatSpec {
     assert(insertion)
   }
 
+  it should "insert different figis" in {
+    val candles = List(
+      Candle(
+        FiniteDuration(3, SECONDS),
+        Figis.BTC,
+        CandleDetails(25123.97, 45864.23, 31653.55, 42777.15, Instant.now())
+      ),
+      Candle(
+        FiniteDuration(3, SECONDS),
+        Figis.ETH,
+        CandleDetails(1.25, 2.25, 5.78, 9.34, Instant.now())
+      ),
+      Candle(
+        FiniteDuration(3, SECONDS),
+        Figis.DOGE,
+        CandleDetails(0.53, 0.72, 0.65, 0.68, Instant.now())
+      ),
+    )
+
+    val insertions = candles.map(candle => RedisCandleDao.insertCandle(candle))
+
+    assert(insertions.reduce(_ && _))
+  }
+
 }
